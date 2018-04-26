eeae37e20fd13d122031a8e97f43087aafd73ac5
Du Lam <u103034@DBCC02Q38XFG8WPDu-Lam.local>
2018-04-26 09:13:07 -0700

    need to add makefile

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 #gcc -I ./minilibx_macos  -L ./minilibx_macos -l mlx -framework OpenGL -framework AppKit
-gcc  -lpthread -Wall -Wextra -Werror src/*.c -I ./includes -I ./libs/libft/includes -L ./libs/libft/ -l ft -I ./libs/minilibx_macos -L ./libs/minilibx_macos -l mlx -framework OpenGL -framework AppKit -o fractol
+gcc  -Wall -Wextra -Werror src/*.c -I ./includes -I ./libft/includes -L ./libft/ -l ft -I ./minilibx_macos -L ./minilibx_macos -l mlx -framework OpenGL -framework AppKit -o fractol
